d968eee74333672b168d43d29f491526a0882452
abhradipg <abhradip.ghosh99@gmail.com>
2022-11-25 19:00:33 +0530

    checking

--- PartB/header/gpu_thread.h
+++ PartB/header/gpu_thread.h
@@ -10,17 +10,38 @@ const int SHMEM_SIZE = 32*32*2;
     int colC = blockIdx.x * blockDim.x + threadIdx.x;
     int row=rowC<<1;
     int col=colC<<1;
-    int temp=0;
-
+    
+    __shared__ int s_a[SHMEM_SIZE];
+    __shared__ int s_b[SHMEM_SIZE];
+    
     if( row>=N || col >=N ) return;
 
-    for (int iter = 0; iter < N; iter++) {
-       temp += a[row * N + iter] * b[iter * N + col];
-       temp += a[row * N + iter] * b[iter * N + col+1];
-       temp += a[(row+1) * N + iter] * b[iter * N + col];
-       temp += a[(row+1) * N + iter] * b[iter * N + col+1];
+    int temp=0;
+    int blockx=blockDim.x*2;
+    for (int i = 0; i < N>>1; i += blockDim.x) {
+        
+        int2 b_temp1 = reinterpret_cast<int2*>(&b[(i*2) * N + (threadIdx.y * 2) * N + col])[0];
+        int2 b_temp2 = reinterpret_cast<int2*>(&b[(i*2 + 1) * N + (threadIdx.y * 2) * N + col])[0];
+        int2 a_temp1 = reinterpret_cast<int2*>(&a[row * N + (i*2) + (threadIdx.x*2)])[0];
+        int2 a_temp2 = reinterpret_cast<int2*>(&a[(row+1) * N + (i*2) + (threadIdx.x*2)])[0];
+
+        s_a[(threadIdx.y) * blockx + (threadIdx.x*2)] = a_temp1.x+a_temp2.x;
+        s_a[(threadIdx.y) * blockx + (threadIdx.x*2) + 1] = a_temp1.y+a_temp2.y;
+        s_b[threadIdx.y * 2 * blockDim.x + threadIdx.x] = b_temp1.x+b_temp1.y;
+        s_b[(threadIdx.y * 2 + 1) * blockDim.x + threadIdx.x] = b_temp2.x+b_temp2.y;
+
+    __syncthreads();
+
+    // Do matrix multiplication on the small matrix
+    for (int iter = 0; iter < blockx; iter++) {
+      temp  +=s_a[(threadIdx.y) * blockx + iter] * s_b[iter * blockDim.x + threadIdx.x];
     }
+    // Wait for all threads to finish using current tiles before loading in new
+    // ones
+    __syncthreads();
+  }
     c[rowC*(N>>1) + colC]+=temp;
+
 }
 
 // Fill in this function
@@ -59,15 +80,16 @@ __global__ void matrixRedMul(int *a, int *b, int *c, int N) {
     int row=rowC<<1;
     int col=colC<<1;
     int temp=0;
+
+    if( row>=N || col >=N ) return;
+
     for (int iter = 0; iter < N; iter++) {
        temp += a[row * N + iter] * b[iter * N + col];
        temp += a[row * N + iter] * b[iter * N + col+1];
        temp += a[(row+1) * N + iter] * b[iter * N + col];
        temp += a[(row+1) * N + iter] * b[iter * N + col+1];
     }
-    }
     c[rowC*(N>>1) + colC]+=temp;
-
 }*/
 
 /* ijk unrolled
@@ -111,45 +133,6 @@ __global__ void matrixRedMul(int *a, int *b, int *c, int N) {
        temp += a_temp2.w * b_temp4.y;
 
     }
-    c[rowC*(N>>1) + colC]+=temp;
-
-}
-
-__global__ void matrixRedMul(int *a, int *b, int *c, int N) {
-    int rowC = blockIdx.y * blockDim.y + threadIdx.y;
-    int colC = blockIdx.x * blockDim.x + threadIdx.x;
-    int row=rowC<<1;
-    int col=colC<<1;
-    
-    __shared__ int s_a[SHMEM_SIZE];
-    __shared__ int s_b[SHMEM_SIZE];
-    
-    if( row>=N || col >=N ) return;
-
-    int temp=0;
-    int blockx=blockDim.x*2;
-    for (int i = 0; i < N>>1; i += blockDim.x) {
-        
-        int2 b_temp1 = reinterpret_cast<int2*>(&b[(i*2) * N + (threadIdx.y * 2) * N + col])[0];
-        int2 b_temp2 = reinterpret_cast<int2*>(&b[(i*2 + 1) * N + (threadIdx.y * 2) * N + col])[0];
-        int2 a_temp1 = reinterpret_cast<int2*>(&a[row * N + (i*2) + (threadIdx.x*2)])[0];
-        int2 a_temp2 = reinterpret_cast<int2*>(&a[(row+1) * N + (i*2) + (threadIdx.x*2)])[0];
-
-        s_a[(threadIdx.y) * blockx + (threadIdx.x*2)] = a_temp1.x+a_temp2.x;
-        s_a[(threadIdx.y) * blockx + (threadIdx.x*2) + 1] = a_temp1.y+a_temp2.y;
-        s_b[threadIdx.y * 2 * blockDim.x + threadIdx.x] = b_temp1.x+b_temp1.y;
-        s_b[(threadIdx.y * 2 + 1) * blockDim.x + threadIdx.x] = b_temp2.x+b_temp2.y;
-
-    __syncthreads();
-
-    // Do matrix multiplication on the small matrix
-    for (int iter = 0; iter < blockx; iter++) {
-      temp  +=s_a[(threadIdx.y) * blockx + iter] * s_b[iter * blockDim.x + threadIdx.x];
-    }
-    // Wait for all threads to finish using current tiles before loading in new
-    // ones
-    __syncthreads();
-  }
     c[rowC*(N>>1) + colC]+=temp;
 
 }
