709b4137a3fa65a2ed768b98980a4b66d702e14f
abhradipg <abhradip.ghosh99@gmail.com>
2022-11-30 21:47:58 +0530

    final maybe ?

--- PartA/header/multi_thread.h
+++ PartA/header/multi_thread.h
@@ -39,6 +39,29 @@ class Arguments{
    }
 };
 
+void * computeB(void *ptr){
+Arguments *arg=(Arguments *)ptr;
+    
+    int N=arg->getN();
+    int blockSize=arg->getBlockSize();
+    int blockStart=arg->getBlockStart();
+    int *matB=arg->getMatB();
+    int *output=arg->getOutput();
+    int sizeA=N*N;
+    int indexC,sum,rowFirstIndex,rowSecondIndex,rowBIndex,noOfRowC=N>>1;
+    bool isLastBlock=arg->getIsLastBlock();
+    __m256i zeroVector=_mm256_set1_epi32(0);
+    __m256i permuteVector=_mm256_setr_epi32(0,1,4,5,2,3,6,7);
+    __m256i colFirst,colSecond,row,sumTotal;
+    int blockEnd=isLastBlock?N:blockStart+blockSize;
+    for(int rowB = blockStart; rowB < blockEnd; rowB++){
+        for(int col=0;col<N;col+=16){
+      _mm256_storeu_si256((__m256i *)&output[rowB*(noOfRowC)+(col>>1)],_mm256_hadd_epi32(_mm256_loadu_si256((__m256i *)&matB[rowB*N+col]),
+                                                                      _mm256_loadu_si256((__m256i *)&matB[rowB*N+col+8])));
+    }
+      }
+    return NULL;
+}
 
 // Create other necessary functions here
 void * multiplyBlock(void *ptr){
@@ -53,11 +76,13 @@ void * multiplyBlock(void *ptr){
     int sizeA=N*N;
     int sizeC=sizeA>>2;
     int noOfRowC=N>>1;
-    int indexC,sum,rowFirstIndex,rowSecondIndex,rowBIndex;
+    int indexC,sum,rowFirstIndex,rowSecondIndex,rowBIndex,rowB;
     bool isLastBlock=arg->getIsLastBlock();
     __m256i zeroVector=_mm256_set1_epi32(0);
     __m256i permuteVector=_mm256_setr_epi32(0,1,4,5,2,3,6,7);
-    __m256i colFirst,colSecond,row,sumTotal;
+    __m256i colFirst1,colFirst2,colFirst3,colFirst4,colFirst5,colFirst6,colFirst7,colFirst8;
+    __m256i sumTotal1,sumTotal2,sumTotal3,sumTotal4,sumTotal5,sumTotal6,sumTotal7,sumTotal8;
+    __m256i colSecond,row,row1,row2,row3,row4,row5,row6,row7,row8,colC;
     int blockEnd=isLastBlock?N:blockStart+blockSize;
     int rowCStart=(blockStart>>1)*noOfRowC,rowCEnd=(blockEnd>>1)*noOfRowC;
     for(indexC=rowCStart;indexC<rowCEnd;indexC+=8){
@@ -66,18 +91,57 @@ void * multiplyBlock(void *ptr){
     for(int rowA = blockStart, rowC = rowCStart; rowA < blockEnd; rowA+=2,rowC+=noOfRowC){
       rowFirstIndex=rowA * N;
       rowSecondIndex=(rowA+1) * N;
-      for(int iter = 0; iter < N; iter++){
-        row = _mm256_set1_epi32(matA[rowFirstIndex + iter]+matA[rowSecondIndex + iter]);
-        rowBIndex=iter * N;
-        for(int colB = 0, indexC = rowC; colB < N; colB+=16, indexC+=8){
-          colFirst = _mm256_loadu_si256((__m256i *)&matB[rowBIndex + colB]);
-          colSecond = _mm256_loadu_si256((__m256i *)&matB[rowBIndex + (colB+8)]);
-          sumTotal = _mm256_hadd_epi32(_mm256_mullo_epi32(row,colFirst),_mm256_mullo_epi32(row,colSecond));
-        _mm256_storeu_si256((__m256i *)&output[indexC],_mm256_add_epi32(_mm256_loadu_si256((__m256i *)&output[indexC]),sumTotal));
-         }
+      for(int iter = 0; iter < N; iter +=8){
+      row = _mm256_add_epi32(_mm256_loadu_si256((__m256i *)&matA[rowFirstIndex + iter]),
+                             _mm256_loadu_si256((__m256i *)&matA[rowSecondIndex + iter]));
+      int *r=(int *)&row;
+      row1 = _mm256_set1_epi32(r[0]);
+      row2 = _mm256_set1_epi32(r[1]);
+      row3 = _mm256_set1_epi32(r[2]);
+      row4 = _mm256_set1_epi32(r[3]);
+      row5 = _mm256_set1_epi32(r[4]);
+      row6 = _mm256_set1_epi32(r[5]);
+      row7 = _mm256_set1_epi32(r[6]);
+      row8 = _mm256_set1_epi32(r[7]);
+      rowBIndex=iter * noOfRowC;
+      for(int colB = 0, indexC = rowC; colB < noOfRowC; colB+=8, indexC+=8) 
+      { 
+        colC=_mm256_loadu_si256((__m256i *)&output[indexC]);
+        rowB=rowBIndex;
+        colFirst1 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal1 = _mm256_mullo_epi32(row1,colFirst1);
+        colC=_mm256_add_epi32(colC,sumTotal1);
+        rowB+=noOfRowC;
+        colFirst2 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal2 = _mm256_mullo_epi32(row2,colFirst2);
+        colC=_mm256_add_epi32(colC,sumTotal2);
+        rowB+=noOfRowC;
+        colFirst3 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal3 = _mm256_mullo_epi32(row3,colFirst3);
+        colC=_mm256_add_epi32(colC,sumTotal3);
+        rowB+=noOfRowC;
+        colFirst4 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal4 = _mm256_mullo_epi32(row4,colFirst4);
+        colC=_mm256_add_epi32(colC,sumTotal4);
+        rowB+=noOfRowC;
+        colFirst5 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal5 = _mm256_mullo_epi32(row5,colFirst5);
+        colC=_mm256_add_epi32(colC,sumTotal5);
+        rowB+=noOfRowC;
+        colFirst6 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal6 = _mm256_mullo_epi32(row6,colFirst6);
+        colC=_mm256_add_epi32(colC,sumTotal6);
+        rowB+=noOfRowC;
+        colFirst7 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal7 = _mm256_mullo_epi32(row7,colFirst7);
+        colC=_mm256_add_epi32(colC,sumTotal7);
+        rowB+=noOfRowC;
+        colFirst8 = _mm256_loadu_si256((__m256i *)&matB[rowB + colB]);
+        sumTotal8 = _mm256_mullo_epi32(row8,colFirst8);
+        _mm256_storeu_si256((__m256i *)&output[indexC],_mm256_add_epi32(colC,sumTotal8));
+      }
         }
       }
-
     for(indexC=rowCStart;indexC<rowCEnd;indexC+=8){
     _mm256_storeu_si256((__m256i *)&output[indexC],_mm256_permutevar8x32_epi32(_mm256_loadu_si256((__m256i *)&output[indexC]), permuteVector));
   }
@@ -89,7 +153,7 @@ void * multiplyBlock(void *ptr){
 void multiThread(int N, int *matA, int *matB, int *output)
 {
     assert( N>=4 and N == ( N &~ (N-1)));
-    const int noOfThreads=12;
+    const int noOfThreads=8;
     int blockSize=N/noOfThreads,blockStart=0;
     if(blockSize&1)    //Make blockSize even
       {
@@ -117,18 +181,34 @@ void multiThread(int N, int *matA, int *matB, int *output)
    }
    return;
   }
-    pthread_t threadIds[noOfThreads];
-    Arguments *threadVariable=(Arguments *)malloc(noOfThreads*sizeof(Arguments));
+    pthread_t threadId1s[noOfThreads];
+    pthread_t threadId2s[noOfThreads];
+    Arguments *threadVariablePreCompute=(Arguments *)malloc(noOfThreads*sizeof(Arguments));
     //Starting Threads
+    Arguments *threadVariable=(Arguments *)malloc(noOfThreads*sizeof(Arguments));
+    int *matmatB = new int[N*N>>1];
     for(int i=0;i<noOfThreads;i++){
        isLastBlock=(i==noOfThreads-1)?true:false;
-       new (&threadVariable[i]) Arguments(N, matA, matB, output, blockSize, blockStart, isLastBlock);
-       pthread_create( &threadIds[i], NULL, multiplyBlock, (void *)&threadVariable[i] );
+       new (&threadVariablePreCompute[i]) Arguments(N, matA, matB, matmatB, blockSize, blockStart, isLastBlock);
+       pthread_create( &threadId1s[i], NULL, computeB, (void *)&threadVariablePreCompute[i] );
        blockStart=blockStart+blockSize;
     }
     //Waiting for Threads to complete
     for(int i=0;i<noOfThreads;i++){
-      pthread_join(threadIds[i],NULL);
+      pthread_join(threadId1s[i],NULL);
+    }
+    free(threadVariablePreCompute);
+    blockStart=0;
+    for(int i=0;i<noOfThreads;i++){
+       isLastBlock=(i==noOfThreads-1)?true:false;
+       new (&threadVariable[i]) Arguments(N, matA, matmatB, output, blockSize, blockStart, isLastBlock);
+       pthread_create( &threadId2s[i], NULL, multiplyBlock, (void *)&threadVariable[i] );
+       blockStart=blockStart+blockSize;
+    }
+    //Waiting for Threads to complete
+    for(int i=0;i<noOfThreads;i++){
+      pthread_join(threadId2s[i],NULL);
     }
     free(threadVariable);
+    free(matmatB);
 }
--- PartA/header/single_thread.h
+++ PartA/header/single_thread.h
@@ -4,15 +4,24 @@
 void singleThread(int N, int *matA, int *matB, int *output)
 {
   assert( N>=4 and N == ( N &~ (N-1)));
+    int *Bnew = new int[N*(N>>1)];
     int sizeA=N*N;
     int sizeC=sizeA>>2;
     int noOfRowC=N>>1;
-    int indexC,sum,rowFirstIndex,rowSecondIndex,rowBIndex;
+    int indexC,sum,rowFirstIndex,rowSecondIndex,rowBIndex,rowB;
     __m256i zeroVector=_mm256_set1_epi32(0);
     __m256i permuteVector=_mm256_setr_epi32(0,1,4,5,2,3,6,7);
-    __m256i colFirst,colSecond,row,sumTotal;
+    __m256i colFirst1,colFirst2,colFirst3,colFirst4,colFirst5,colFirst6,colFirst7,colFirst8;
+    __m256i sumTotal1,sumTotal2,sumTotal3,sumTotal4,sumTotal5,sumTotal6,sumTotal7,sumTotal8;
+    __m256i colSecond,row,row1,row2,row3,row4,row5,row6,row7,row8,colC;
   for(indexC=0;indexC<sizeC;indexC+=8){
     _mm256_storeu_si256((__m256i *)&output[indexC],zeroVector);
+  }
+  for(int row=0;row<N;row++){
+    for(int col=0;col<N;col+=16){
+      _mm256_storeu_si256((__m256i *)&Bnew[row*(noOfRowC)+(col>>1)],_mm256_hadd_epi32(_mm256_loadu_si256((__m256i *)&matB[row*N+col]),
+                                                                      _mm256_loadu_si256((__m256i *)&matB[row*N+col+8])));
+    }
   }
   if(N<16){
     for(int rowA = 0; rowA < N; rowA +=2) {
@@ -35,21 +44,61 @@ void singleThread(int N, int *matA, int *matB, int *output)
    }
    return;
   }
-  for(int rowA = 0,rowC = 0; rowA < N; rowA+=2, rowC+=noOfRowC) {
+  for(int rowA = 0, rowC = 0; rowA < N; rowA+=2,rowC+=noOfRowC){
     rowFirstIndex=rowA * N;
     rowSecondIndex=(rowA+1) * N;
-    for(int iter = 0; iter < N; iter ++){
-      row = _mm256_set1_epi32(matA[rowFirstIndex + iter]+matA[rowSecondIndex + iter]);
-      rowBIndex=iter * N;
-      for(int colB = 0, indexC = rowC; colB < N; colB+=16, indexC+=8) 
+    for(int iter = 0; iter < N; iter +=8){
+      row = _mm256_add_epi32(_mm256_loadu_si256((__m256i *)&matA[rowFirstIndex + iter]),
+                             _mm256_loadu_si256((__m256i *)&matA[rowSecondIndex + iter]));
+      int *r=(int *)&row;
+      row1 = _mm256_set1_epi32(r[0]);
+      row2 = _mm256_set1_epi32(r[1]);
+      row3 = _mm256_set1_epi32(r[2]);
+      row4 = _mm256_set1_epi32(r[3]);
+      row5 = _mm256_set1_epi32(r[4]);
+      row6 = _mm256_set1_epi32(r[5]);
+      row7 = _mm256_set1_epi32(r[6]);
+      row8 = _mm256_set1_epi32(r[7]);
+      rowBIndex=iter * noOfRowC;
+      for(int colB = 0, indexC = rowC; colB < noOfRowC; colB+=8, indexC+=8) 
       { 
-        colFirst = _mm256_loadu_si256((__m256i *)&matB[rowBIndex + colB]);
-        colSecond = _mm256_loadu_si256((__m256i *)&matB[rowBIndex + (colB+8)]);
-        sumTotal = _mm256_hadd_epi32(_mm256_mullo_epi32(row,colFirst),_mm256_mullo_epi32(row,colSecond));
-        _mm256_storeu_si256((__m256i *)&output[indexC],_mm256_add_epi32(_mm256_loadu_si256((__m256i *)&output[indexC]),sumTotal));
+        colC=_mm256_loadu_si256((__m256i *)&output[indexC]);
+        rowB=rowBIndex;
+        colFirst1 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal1 = _mm256_mullo_epi32(row1,colFirst1);
+        colC=_mm256_add_epi32(colC,sumTotal1);
+        rowB+=noOfRowC;
+        colFirst2 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal2 = _mm256_mullo_epi32(row2,colFirst2);
+        colC=_mm256_add_epi32(colC,sumTotal2);
+        rowB+=noOfRowC;
+        colFirst3 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal3 = _mm256_mullo_epi32(row3,colFirst3);
+        colC=_mm256_add_epi32(colC,sumTotal3);
+        rowB+=noOfRowC;
+        colFirst4 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal4 = _mm256_mullo_epi32(row4,colFirst4);
+        colC=_mm256_add_epi32(colC,sumTotal4);
+        rowB+=noOfRowC;
+        colFirst5 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal5 = _mm256_mullo_epi32(row5,colFirst5);
+        colC=_mm256_add_epi32(colC,sumTotal5);
+        rowB+=noOfRowC;
+        colFirst6 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal6 = _mm256_mullo_epi32(row6,colFirst6);
+        colC=_mm256_add_epi32(colC,sumTotal6);
+        rowB+=noOfRowC;
+        colFirst7 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal7 = _mm256_mullo_epi32(row7,colFirst7);
+        colC=_mm256_add_epi32(colC,sumTotal7);
+        rowB+=noOfRowC;
+        colFirst8 = _mm256_loadu_si256((__m256i *)&Bnew[rowB + colB]);
+        sumTotal8 = _mm256_mullo_epi32(row8,colFirst8);
+        _mm256_storeu_si256((__m256i *)&output[indexC],_mm256_add_epi32(colC,sumTotal8));
       }
     }
   }
+
   for(indexC=0;indexC<sizeC;indexC+=8){
     _mm256_storeu_si256((__m256i *)&output[indexC],_mm256_permutevar8x32_epi32(_mm256_loadu_si256((__m256i *)&output[indexC]), permuteVector));
   }
